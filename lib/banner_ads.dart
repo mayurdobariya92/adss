30950e0a66da8e6271d416b5c77b3aabe3e2c87c
yashkumbhani1147 <yashkumbhani1147@example.com>
2022-10-03 09:49:10 +0530

    Initial commit

---
+++ lib/banner_ads.dart
@@ -0,0 +1,54 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+class banner_ads extends StatefulWidget {
+  const banner_ads({Key? key}) : super(key: key);
+
+  @override
+  State<banner_ads> createState() => _banner_adsState();
+}
+
+class _banner_adsState extends State<banner_ads> {
+  BannerAd?_bannerAd;
+  bool _bannerAdIsLoaded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Banner Ads"),),
+      body: _bannerAdIsLoaded? Container(
+        height: _bannerAd!.size.height.toDouble(),
+        width: _bannerAd!.size.width.toDouble(),
+          child: AdWidget(ad: _bannerAd!))
+      :Container(),
+    );
+  }
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    // Create the ad objects and load ads.
+    _bannerAd = BannerAd(
+        size: AdSize.banner,
+        adUnitId: Platform.isAndroid
+            ? 'ca-app-pub-3940256099942544/6300978111'
+            : 'ca-app-pub-3940256099942544/2934735716',
+        listener: BannerAdListener(
+          onAdLoaded: (Ad ad) {
+            print('$BannerAd loaded.');
+            setState(() {
+              _bannerAdIsLoaded = true;
+            });
+          },
+          onAdFailedToLoad: (Ad ad, LoadAdError error) {
+            print('$BannerAd failedToLoad: $error');
+            ad.dispose();
+          },
+          onAdOpened: (Ad ad) => print('$BannerAd onAdOpened.'),
+          onAdClosed: (Ad ad) => print('$BannerAd onAdClosed.'),
+        ),
+        request: AdRequest())
+      ..load();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:adss/banner_ads.dart';
+import 'package:adss/interads.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+  runApp(MaterialApp(home: interads(),));
+}
+const int maxFailedLoadAttempts = 3;
+
+
+class ads extends StatefulWidget {
+  const ads({Key? key}) : super(key: key);
+
+  @override
+  State<ads> createState() => _adsState();
+}
+
+class _adsState extends State<ads> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Advertisment"),),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(onPressed: () {
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+                return banner_ads();
+              },));
+            }, child: Text("Baner Ads")),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
